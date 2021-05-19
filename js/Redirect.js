0e2ed406f58b8aed2acff845bef41329e82ae89b
Kevin Palencia <kevingil1910@gmail.com>
2021-05-19 17:50:05 -0600

    catologo carrusel FREDY MARCOS

---
+++ js/Redirect.js
@@ -0,0 +1,16 @@
+function RedirectDiv(link){
+    switch(link){
+        case 1:  window.location.href ="http://www.facebook.com";
+            break;
+        case 2:  window.location.href ="http://www.facebook.com";
+            break;
+        case 3:  window.location.href ="http://www.facebook.com";
+            break;
+        case 4:  window.location.href ="http://www.facebook.com";
+            break;
+        case 5:  window.location.href ="http://www.facebook.com";
+            break;
+        default: window.location.href ="https://www.twitter.com";
+    }
+
+}
